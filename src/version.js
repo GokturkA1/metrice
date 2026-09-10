eb4334f9ed88449ea6eea639157fa3aff8472c00
GokturkA1 <gokturk@gokturka.net>
2026-09-10 19:25:41 +0300

    chore(release): v2.5.9

--- src/version.js
+++ src/version.js
@@ -7,7 +7,7 @@ import { readFileSync } from 'node:fs';
  * kod tabanında veya testlerde elle versiyon dizgesi güncelleme ihtiyacını ortadan kaldırır.
  */
 
-let packageVersion = '2.5.8';
+let packageVersion = '2.5.9';
 try {
   const packageJsonUrl = new URL('../package.json', import.meta.url);
   const pkg = JSON.parse(readFileSync(packageJsonUrl, 'utf-8'));
